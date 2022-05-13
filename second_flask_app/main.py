565d41e46ae8ca2e6e4cf7db2da1ea26294f8ba1
nicolas <nicolas@enlaps.fr>
2022-05-13 17:51:33 +0200

    feat(second app): add new methods

--- second_flask_app/main.py
+++ second_flask_app/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
+import werkzeug
 
 app = Flask(__name__)
 
@@ -6,6 +8,27 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return render_template('home.html')
+
+
+@app.route('/tikee/<int:tikee_id>', methods=['POST'])
+def tikee_page(tikee_id):
+    print(request.form['text1'])
+    # return render_template('tikee.html', tikee_id=tikee_id)
+
+
+@app.route('/project/<int:project_id>')
+def project_page(project_id):
+    return render_template('project.html', project_id=project_id)
+
+
+@app.route('/enlaps')
+def enlaps_page():
+    return render_template('enlaps.html')
+
+
+@app.errorhandler(werkzeug.exceptions.HTTPException)
+def bad_request(e):
+    return render_template('error.html', e=e)
 
 
 if __name__ == '__main__':
